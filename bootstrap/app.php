91d3d17785c3342f36068cb58ffd137eb305a6f9
Mauricio <mauricio.montoyav@gmail.com>
2025-07-31 19:07:54 -0500

    Add additional Cloudflare IP ranges to trusted proxies middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,34 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
+        // Trust specific proxy IPs - Cloudflare IP ranges
+        $middleware->trustProxies(
+            at: env('TRUSTED_PROXIES') ? 
+                explode(',', env('TRUSTED_PROXIES')) : 
+                [
+                    // Cloudflare IPv4 ranges (principales)
+                    '103.21.244.0/22',
+                    '103.22.200.0/22', 
+                    '103.31.4.0/22',
+                    '104.16.0.0/13',
+                    '104.24.0.0/14',
+                    '108.162.192.0/18',
+                    '131.0.72.0/22',
+                    '141.101.64.0/18',
+                    '162.158.0.0/15',
+                    '172.64.0.0/13',
+                    '173.245.48.0/20',
+                    '188.114.96.0/20',
+                    '190.93.240.0/20',
+                    '197.234.240.0/22',
+                    '198.41.128.0/17',
+                    // Laravel Cloud internal network
+                    '10.0.0.0/8',
+                    '172.16.0.0/12',
+                    '192.168.0.0/16',
+                ]
+        );
+
         $middleware->web(append: [
             // SecurityHeadersMiddleware::class, // Temporarily disabled for testing
             AuditLogMiddleware::class,
