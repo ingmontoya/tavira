7cb44c5e77834c90e2d675bd2649c8331fce2d87
Mauricio <mauricio.montoyav@gmail.com>
2025-09-01 20:59:11 -0500

    fix: update user authentication checks to use optional chaining

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -25,7 +25,7 @@ export interface NavItem {
 export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     name: string;
     quote: { message: string; author: string };
-    auth: Auth;
+    auth?: Auth;
     ziggy: Config & { location: string };
     sidebarOpen: boolean;
 };
